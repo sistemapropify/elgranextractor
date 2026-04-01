839e0218f53410c46981df2d434dbfbe676389db
propifygit <sistemapropify@gmail.com>
2026-04-01 13:39:47 -0500

    Comment out analisis_crm.urls due to ModuleNotFoundError in production

--- webapp/urls.py
+++ webapp/urls.py
@@ -72,7 +72,7 @@ urlpatterns = [
     path('market-analysis/', include('market_analysis.urls')),
     
     # Análisis CRM - Dashboard de leads
-    path('analisis-crm/', include('analisis_crm.urls')),
+    # path('analisis-crm/', include('analisis_crm.urls')),  # Comentado temporalmente debido a error de importación en producción
     
     # Meta Ads - Dashboard de campañas publicitarias de Meta
     # path('meta-ads/', include('meta_ads.urls')),  # Comentado temporalmente debido a error de importación en producción
